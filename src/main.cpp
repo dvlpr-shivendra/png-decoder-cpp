0f7ebdcc6e5486d39f3f43802d56763464b3eb09
Shivendra TechSter <shivendratechSter@gmail.com>
2023-04-26 23:22:18 +0530

    use fltk to display image

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,60 @@
+#include <FL/Fl.H>
+#include <FL/fl_draw.H>
+#include <FL/Fl_Window.H>
+
+#include <cstdint>
+#include <cstdlib>
+
 #include "Decoder.h"
 #include "Streamer.h"
 #include <iostream>
-#include <sys/ioctl.h>
-#include <cmath>
+
+class ImageWindow : public Fl_Window
+{
+public:
+    ImageWindow(int w, int h) : Fl_Window(w, h)
+    {
+    }
+
+    virtual void draw()
+    {
+        double rows_count = h();
+        double cols_count = w();
+
+        double height = image.getHeight();
+        double width = image.getWidth();
+
+        if (height > rows_count || width > cols_count)
+        {
+            double new_height = rows_count;
+            double new_width = cols_count;
+
+            if (height > new_height)
+            {
+                new_width = width / height * new_height;
+            }
+
+            if (width > new_width)
+            {
+                new_height = height / width * new_width;
+            }
+
+            image.resize(new_height, new_width);
+        }
+
+        for (size_t y = 0; y < image.getHeight(); y++)
+        {
+            for (size_t x = 0; x < image.getWidth(); x++)
+            {
+                Pixel pixel = image.getPixel(x, y);
+                fl_color(pixel.r, pixel.g, pixel.b);
+                fl_point(x, y);
+            }
+        }
+    }
+
+    Image image;
+};
 
 int main(int argc, char *argv[])
 {
@@ -13,43 +65,19 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    struct winsize size;
-    ioctl(fileno(stdout), TIOCGWINSZ, &size);
-    int rows_count = size.ws_row;
-    int cols_count = size.ws_col;
+    int rows_count = 640;
+    int cols_count = 480;
 
     Decoder decoder;
 
     Image image = decoder.decode(argv[1]);
 
-    double height = image.getHeight();
-    double width = image.getWidth();
+    ImageWindow window(640, 480);
 
-    if (height > rows_count || width > cols_count)
-    {
-        double new_height = rows_count;
-        double new_width = cols_count;
+    window.image = image;
 
-        if (height > new_height)
-        {
-            new_width = width / height * new_height;
-        }
+    window.end();
+    window.show(argc, argv);
 
-        if (width > new_width)
-        {
-            new_height = height / width * new_width;
-        }
-
-        image.resize(new_height, new_width);
-    }
-
-    for (size_t y = 0; y < image.getHeight(); y++)
-    {
-        for (size_t x = 0; x < image.getWidth(); x++)
-        {
-            Pixel pixel = image.getPixel(x, y);
-            std::cout << "\033[48;2;" << pixel.r << ";" << pixel.g << ";" << pixel.b << "m  ";
-        }
-        std::cout << "\033[0m" << std::endl;
-    }
+    return Fl::run();
 }
